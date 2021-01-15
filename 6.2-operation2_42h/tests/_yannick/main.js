acaff0db39f8e990dcaaab78769ad4a24a8c209e
Yannick <vynk79@gmail.com>
2021-01-15 11:20:28 +0100

    Update yannick

---
+++ 6.2-operation2_42h/tests/_yannick/main.js
@@ -0,0 +1,7 @@
+if (localStorage.getItem('user_mail')) {
+    document.getElementById('connectButton').style.display = "none";
+    document.getElementById('userMsg').style.display = "block";
+}
+else {
+    
+}
