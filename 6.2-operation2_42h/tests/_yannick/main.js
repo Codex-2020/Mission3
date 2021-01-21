bf110386578299e0629e08ac675b69be96339b72
Yannick <vynk79@gmail.com>
2021-01-21 14:17:00 +0100

    Delete

--- 6.2-operation2_42h/tests/_yannick/main.js
+++
@@ -1,7 +0,0 @@
-if (localStorage.getItem('user_mail')) {
-    document.getElementById('connectButton').style.display = "none";
-    document.getElementById('userMsg').style.display = "block";
-}
-else {
-    
-}
