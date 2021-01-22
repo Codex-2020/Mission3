2020821f531b8d765c4eff4fb535e1f9e9de9575
Alain-sys <Alain-sys@protonmail.com>
2021-01-22 01:09:12 +0100

    update repack

--- 6.2-operation2_42h/renduFinal/js/payment.js
+++ 6.2-operation2_42h/renduFinal/js/payment.js
@@ -6,8 +6,8 @@
 //en attendant que toutes les pages soient communes
 // localStorage.setItem("user_firstname", "Nissim");
 // localStorage.setItem("user_name", "Djerouri");
-// localStorage.setItem("user_addres", "56 rue des daltoniens");
-// localStorage.setItem("user_city", "75000 Silicon Valley");
+localStorage.setItem("user_addres", "56 rue des daltoniens");
+localStorage.setItem("user_city", "75000 Silicon Valley");
 // sessionStorage.setItem("user_cart", "120");
 
 
